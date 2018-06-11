1f7177a7846b7d146c2631b9e249da7479648706
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-06-10 22:54:45 -0700

    completed featureSet1&2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//this does not hoist......
+var luckyNumber = () => Math.floor(Math.random()*1000);
+
+var greeting = (userObject) => {
+    const element = (
+        <div className="container">
+            <h1>Welcome {userObject.name}</h1>
+            <h2 className="text-muted">Your lucky number is: {userObject.luckyNum}</h2>
+        </div>
+    );
+    return element;
+}
+
+let name = "Peter";
+let luckyNum = luckyNumber();
+const user = {name, luckyNum};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting( user ),
     document.getElementById('root')
 );
